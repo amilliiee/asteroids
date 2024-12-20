5ba692fc7f3c9942262ea426d70226ce68abf839
Amelia <alsylvester@hotmail.com>
2024-12-20 12:36:40 -0700

    create player icon

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import *
+from player import Player
+
+clk  = pygame.time.Clock()
+dt = 0
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player  = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
@@ -11,7 +16,9 @@ def main():
                 return
         
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
+        dt = clk.tick(60) / 1000
 
 
 if __name__ == "__main__":
